b490b4ea27fc15b4af1db5769c1ffff825de8779
Brooke Jones <bjones782@gmail.com>
2022-05-23 13:42:33 -0400

    commit tree 4a0da55576c71cdd94501e06dd568ae20b3c14b1

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,26 @@
 class Movie < ActiveRecord::Base
 
+    def self.create_with_title(my_title)
+        self.create(title: my_title)
+    end
+
+    def self.first_movie
+        Movie.first
+    end
+
+    def self.last_movie
+        Movie.last
+    end
+
+    def self.movie_count
+        Movie.count
+    end
+
+    def self.find_movie_with_id(id)
+        Movie.find(id)
+    end
+
+    def self.find_movie_with_attributes(title)
+        Movie.find_by(title: title)
+    end
 end
